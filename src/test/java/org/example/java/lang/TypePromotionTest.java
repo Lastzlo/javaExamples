ec8cb4e9af15591a436ebd5bf823b53d28401baf
Lastzlo <z.last.zlo@gmail.com>
2021-05-27 13:02:42 +0300

    choir(java\lang\TypePromotionTest.java) renamed from BinaryNumericPromotionTest

--- src/test/java/org/example/java/lang/TypePromotionTest.java
+++ src/test/java/org/example/java/lang/TypePromotionTest.java
@@ -3,7 +3,7 @@ package org.example.java.lang;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class BinaryNumericPromotionTest {
+public class TypePromotionTest {
 
 
     /**
