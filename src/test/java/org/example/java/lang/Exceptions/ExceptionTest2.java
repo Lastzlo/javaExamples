54c2e9562720f3350ca10245795fed0ab7fe1ccf
Lastzlo <z.last.zlo@gmail.com>
2021-06-05 15:07:28 +0300

    test(java\lang\Exceptions\ExceptionTest2) add tests msg, print_whenNeedDeclareException, print_whenDontNeedDeclareException

--- src/test/java/org/example/java/lang/Exceptions/ExceptionTest2.java
+++ src/test/java/org/example/java/lang/Exceptions/ExceptionTest2.java
@@ -1,26 +1,67 @@
 package org.example.java.lang.Exceptions;
 
+import org.junit.jupiter.api.Test;
+
 /**ExceptionHandling with MethodOverriding in Java*/
 public class ExceptionTest2 {
 }
 
 
 class Parent{
-    void msg(){System.out.println("parent");}
+    void msg(){System.out.println("Parent msg");}
+
+    void print() throws Exception {System.out.println("Parent print");}
+
 }
 
-/**1) Rule: If the superclass method does not declare an exception,
- * subclass overridden method cannot declare the checked exception.*/
+
 class TestExceptionChild extends Parent{
+
+    /**1) Rule: If the superclass method does not declare an exception,
+     * subclass overridden method cannot declare the checked exception.*/
     //Compile Time Error, because method throw in Parent does not throws IOException
 //    void msg()throws IOException {
 //        System.out.println("TestExceptionChild");
 //    }
 
-    public static void main(String args[]){
+    @Test
+    public void msg() {
         Parent p=new TestExceptionChild();
-        p.msg();
+        p.msg();    //Parent msg
     }
+
+    /**Subclass overridden method can be without exception*/
+    void print() {
+        System.out.println("Child print");
+    }
+
+    @Test
+    public void print_whenNeedDeclareException() {
+
+        //we want to declare try{} catch OR throws Exception in head,
+        //because parent.print() have throws Exception
+        try {
+            Parent parent = new TestExceptionChild();   //!!!
+            parent.print();   //Child print
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void print_whenDontNeedDeclareException() {
+        //we dont declare try{} catch OR throws Exception in head,
+        //because child.print() have not throws Exception
+        TestExceptionChild child = new TestExceptionChild();
+        child.print();    //Child print
+    }
+
+
+
+
 }
+
+
+
 
 
