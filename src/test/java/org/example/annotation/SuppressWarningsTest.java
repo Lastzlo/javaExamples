b368d1b275df10245a441fd826c067c5c779b6c1
Lastzlo <z.last.zlo@gmail.com>
2021-06-19 11:12:04 +0300

    refactor(annotation\SuppressWarningsTest) rewrite exampleUncheckedWarning, add exampleUncheckedWarning_withAnnotation

--- src/test/java/org/example/annotation/SuppressWarningsTest.java
+++ src/test/java/org/example/annotation/SuppressWarningsTest.java
@@ -2,41 +2,29 @@ package org.example.annotation;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TreeSet;
 
 public class SuppressWarningsTest {
-
-    //нету ошибки
-    // а должна была быть
-    @Test
-    public void addVersion_withoutAnnotation() {
-        new Machine().addVersion("word");
-
-    }
-
     /*Example (of unchecked warning in conjunction with raw types)*/
-    //нету ошибки warning: [unchecked] unchecked call to add(E) as a member of the raw type java.util.TreeSet
-    //               set.add("abc");
-    // а должна была быть
     @Test
     public void exampleUncheckedWarning() {
         TreeSet set = new TreeSet();
+        //warning:
+        // [unchecked] unchecked call to add(E) as a member of the raw type java.util.TreeSet
         set.add("abc");        // unchecked warning
+        set.remove("abc");
+    }
+
+    @Test
+    // If we comment below annotation, program generates
+    // warning
+    @SuppressWarnings("unchecked")
+    public void exampleUncheckedWarning_withAnnotation() {
+        TreeSet set = new TreeSet();
+        set.add("abc");
         set.remove("abc");
     }
 }
 
-class Machine {
-    private List versions;
 
-    public Machine() {
-        versions = new ArrayList();
-    }
-
-    public void addVersion(String version) {
-        versions.add(version);
-    }
-}
 
