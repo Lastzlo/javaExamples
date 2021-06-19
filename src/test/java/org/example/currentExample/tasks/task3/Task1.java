744e6525c86537f40a18eae190f993266b9529b5
Lastzlo <z.last.zlo@gmail.com>
2021-06-19 09:56:26 +0300

    refactor(java\util\concurent\tasks\Task1.java) move to \currentExample\tasks\task3

--- src/test/java/org/example/currentExample/tasks/task3/Task1.java
+++ src/test/java/org/example/currentExample/tasks/task3/Task1.java
@@ -1,12 +1,10 @@
-package org.example.java.util.concurent.tasks;
+package org.example.currentExample.tasks.task3;
 
 import org.junit.jupiter.api.Test;
 
 public class Task1 {
     @Test
     void whatResult() {
-
-
         String str = "I lear Java";
         Runnable r = System.out::println;
         System.out.print("it's well");
