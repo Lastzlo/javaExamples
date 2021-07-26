f9cc2a720ff76843fff563e65c35aa1a22574b9f
Lastzlo <z.last.zlo@gmail.com>
2021-07-26 17:03:25 +0300

    refactor(innerClasses\tasks\task1\Example.java) whenCreateParent, whenCreateParentButSetChild,

--- src/test/java/org/example/oop/innerClasses/tasks/task1/Example.java
+++ src/test/java/org/example/oop/innerClasses/tasks/task1/Example.java
@@ -6,6 +6,8 @@ public class Example {
     @Test
     void whenCreateParent() {
         Parent p = new Example().new Parent();
+        //or
+        Example.Parent p2 = new Example().new Parent();
 
         System.out.println(p.method()); // 10
         System.out.println(((Parent) p).method());  // 10
@@ -15,6 +17,8 @@ public class Example {
     @Test
     void whenCreateParentButSetChild() {
         Parent p = new Example().new Child();
+        //or
+        Example.Parent p2 = new Example().new Child();
 
         System.out.println(p.method()); // 20
         System.out.println(((Parent) p).method());  // 20
@@ -24,6 +28,8 @@ public class Example {
     @Test
     void whenCreateChild() {
         Child p = new Example().new Child();
+        //or
+        Example.Child p2 = new Example().new Child();
 
         System.out.println(p.method()); // 20
         System.out.println(((Parent) p).method());  // 20
