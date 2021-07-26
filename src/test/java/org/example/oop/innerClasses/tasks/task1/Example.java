d65fdf377aa6d009f201baf2278aa5f8d1e06750
Lastzlo <z.last.zlo@gmail.com>
2021-07-26 15:53:05 +0300

    test (innerClasses\tasks\task1\Example.java) add whenCreateParent, whenCreateParentButSetChild, whenCreateChild

---
+++ src/test/java/org/example/oop/innerClasses/tasks/task1/Example.java
@@ -0,0 +1,52 @@
+package org.example.oop.innerClasses.tasks.task1;
+
+import org.junit.jupiter.api.Test;
+
+public class Example {
+    @Test
+    void whenCreateParent() {
+        Parent p = new Example().new Parent();
+
+        System.out.println(p.method()); // 10
+        System.out.println(((Parent) p).method());  // 10
+        //System.out.println(((Child) p).method());   // ClassCastException
+    }
+
+    @Test
+    void whenCreateParentButSetChild() {
+        Parent p = new Example().new Child();
+
+        System.out.println(p.method()); // 20
+        System.out.println(((Parent) p).method());  // 20
+        System.out.println(((Child) p).method());   // 20
+    }
+
+    @Test
+    void whenCreateChild() {
+        Child p = new Example().new Child();
+
+        System.out.println(p.method()); // 20
+        System.out.println(((Parent) p).method());  // 20
+        System.out.println(((Child) p).method());   // 20
+    }
+
+    public static void main(String[] args) {
+        Parent p = new Example().new Child();
+
+        System.out.println(p.method()); // 20
+        System.out.println(((Parent) p).method());  // 20
+        System.out.println(((Child) p).method());   // 20
+    }
+
+    class Parent {
+        int method() {
+            return 10;
+        }
+    }
+
+    class Child extends Parent {
+        int method() {
+            return 20;
+        }
+    }
+}
