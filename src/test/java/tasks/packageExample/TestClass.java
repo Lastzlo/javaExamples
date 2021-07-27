532f1bc4b096cd423f859ba91f3dfd44c1670d9b
Lastzlo <z.last.zlo@gmail.com>
2021-07-27 11:30:39 +0300

    choir (tasks\packageExample) TestClass.java

--- src/test/java/tasks/packageExample/TestClass.java
+++ src/test/java/tasks/packageExample/TestClass.java
@@ -1,9 +1,13 @@
 package tasks.packageExample;
 
-import tasks.packageExample.innerPackage.Student;
+//import tasks.packageExample.innerPackage.Student;
+//or
+import tasks.packageExample.innerPackage.*;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+//or
+//import java.util.List;
+//import java.util.Set;
 
 public class TestClass {
     public void print(List<Student> students){}
