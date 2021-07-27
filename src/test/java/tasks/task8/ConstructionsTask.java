36e1a54719cbfda1b8f10fd0f4b170940d93c7d7
Lastzlo <z.last.zlo@gmail.com>
2021-07-27 20:29:55 +0300

    test (tasks\task8) add ConstructionsTask.java

---
+++ src/test/java/tasks/task8/ConstructionsTask.java
@@ -0,0 +1,16 @@
+package tasks.task8;
+
+import org.junit.jupiter.api.Test;
+
+public class ConstructionsTask {
+    @Test
+    void whatWontCompiled() {
+        long b = 254L;
+        //b.length();
+
+        String country = "null";
+//        float a = 5d;
+
+        country.length();
+    }
+}
