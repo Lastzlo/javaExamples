188e7471c611ba7a1096dc7581ccd454da5f52c5
Lastzlo <z.last.zlo@gmail.com>
2021-09-28 20:14:19 +0300

    feat add assertFalse

--- src/test/java/tasks/quizful/type4/questions.java
+++ src/test/java/tasks/quizful/type4/questions.java
@@ -2,6 +2,8 @@ package tasks.quizful.type4;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class questions {
 
     @Test
@@ -9,6 +11,9 @@ public class questions {
         String s1 = "str";
         String s2 = "str";
         System.out.println("Result: " + s1 == s2);
+
+        boolean isFalse = s2 + s1 == s2 + s1;
+        assertFalse(isFalse);
         //Пояснение: Операции сравнения имеют более низкий приоритет, чем операция конкатенации.
     }
 }
